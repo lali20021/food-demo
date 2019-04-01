ffe0317befdcdc6fc08d1ab627feb69f6e1feb81
Lali Gabelaya <laligabelaya@gmail.com>
2019-03-31 20:10:28 -0700

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "food-demo_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://food-demo-lali.herokuapp.com/' }
+  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
